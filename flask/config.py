d3ff65a8d1930ad68ed26baed823cfb183b2d1e4
Matt Johnson <mjohn23@gmail.com>
2015-05-01 16:16:44 -0500

    Initial database setup

--- flask/config.py
+++ flask/config.py
@@ -1,5 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
+# database config -- replace this with server you want to use
+SQLALCHEMY_DATABASE_URI = 'mysql://hackathonroot:hb2ntCMypcQo@hackathon-mysql.seiuaws.org:3306/hackathon'
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'this-is-a-secret'
