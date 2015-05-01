0d2df9a4adc55b336f92c1caf6e2a75239458815
Matt Johnson <mjohn23@gmail.com>
2015-05-01 16:26:01 -0500

    Model out tables add migration script

--- flask/app/models.py
+++ flask/app/models.py
@@ -6,3 +6,26 @@ class User(db.Model):
 	
 	def __repr__(self):
 		return '<User %t>' % (self.email)
+
+class ReportType(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(120), primary_key=True, unique=True)
+
+class ReportDetails(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	report_type_id = db.Column(db.Integer, db.ForeignKey('reporttype.id'))
+	timestamp = db.Column(db.DateTime)
+	as_of_date = db.Column(db.DateTime)
+	universe_count = db.Column(db.Integer)
+	full_payers_count = db.Column(db.Integer)
+	partial_payers_count = db.Column(db.Integer)
+	member_card_signers_count = db.Column(db.Integer)
+	political_payers_count = db.Column(db.Integer)
+	political_signers_count = db.Column(db.Integer)
+	mailing_address_count = db.Column(db.Integer)
+	work_email_count = db.Column(db.Integer)
+	home_email_count = db.Column(db.Integer)
+	sms_authorization_count = db.Column(db.Integer)
+	home_phone_count = db.Column(db.Integer)
+	cell_phone_count = db.Column(db.Integer)
