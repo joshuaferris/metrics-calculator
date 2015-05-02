8dac3594c43974bf6576b3dfb069b5390bc87773
Matt Johnson <mjohn23@gmail.com>
2015-05-02 09:04:21 -0500

    Make sure form validates

--- flask/app/views.py
+++ flask/app/views.py
@@ -7,4 +7,6 @@ def index():
 	form = UnionMetricsForm()
 	report_types = models.ReportType.query.all()
 	form.report_type.choices = [(rt.id, rt.name) for rt in report_types]
+	if form.validate_on_submit():
+		return render_template('success.html', form=form)
 	return render_template('index.html', form=form)
